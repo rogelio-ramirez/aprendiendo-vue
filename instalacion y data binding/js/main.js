c22fc2b43b64c8ecac44d3f0ac4b10239753cc65
rogelio-ramirez <rogelioramirezr@gmail.com>
2018-12-08 14:16:23 -0600

    Archivos para demostrar la instalación mediante CDN de Vue.js como biblioteca

---
+++ instalacion y data binding/js/main.js
@@ -0,0 +1,11 @@
+// Vanilla JS
+// let input2 = document.getElementById('texto-titulo');
+// let titulo = document.getElementById('titulo');
+
+// input2.addEventListener('keyup', () => titulo.innerText = input2.value);
+new Vue({
+    el: 'main',
+    data: {
+        mensaje: '¡Hola mundo!'
+    }
+});
